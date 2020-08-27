b8f99955ad818c07b98bbd629b913193de0e029b
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-26 20:33:42 -0700

    instantiated path

--- Develop/notetaker.js
+++ Develop/notetaker.js
@@ -1,8 +1,16 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
 var PORT = 3030;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, function () {
   console.log("App listening on PORT" + PORT);
